338b12933145c4a9949af258dc009ff260be239b
DaleStevenJohnson <dalestevenjohnson@gmail.com>
2020-10-23 23:25:00 +0100

    Added code to structure individual inputs and answers.

--- TheJohnsonFilesApplication/Student.cs
+++ TheJohnsonFilesApplication/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace TheJohnsonFilesApplication
@@ -12,10 +13,33 @@ namespace TheJohnsonFilesApplication
             FirstName = data[1];
             Surname = data[2];
             Name = FirstName + " " + Surname;
+            Puzzles = GetPuzzles(data);
         }
+        
         public string FirstName;
         public string Surname;
         public string Name;
         public string ID;
+        internal Dictionary<string, Dictionary<string, string>> Puzzles;
+
+        private Dictionary<string, Dictionary<string, string>> GetPuzzles(string[] data)
+        {
+            Dictionary<string, Dictionary<string, string>> puzzles = new Dictionary<string, Dictionary<string, string>> 
+            {
+                { "Puzzle1", GetPuzzle(data[3], data[4]) },
+                { "Puzzle2", GetPuzzle(data[5], data[6]) },
+            };
+            return puzzles;
+        }
+
+        private Dictionary<string, string> GetPuzzle(string seed, string answer)
+        {
+            Dictionary<string, string> puzzle = new Dictionary<string, string>
+            {
+                { "Seed", seed },
+                { "Answer", answer },
+            };
+            return puzzle;
+        }
     }
 }
